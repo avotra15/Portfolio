a4a2a5df68d236d27d047f330e5220c36d4b3569
Avotra Stanislas <avotraStani@gmail.com>
2026-01-27 08:55:03 +0100

    work page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Work } from './work/work';
+import { Skills } from './skills/skills';
+import { Aboutme } from './aboutme/aboutme';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'home', component: Home},
     {path: 'work', component: Work},
+    {path: 'skills', component: Skills},
+    {path: 'aboutme', component: Aboutme},
+    {path: 'contact', component: Contact},
 ];
