552f36e3bc38e8eba4798ed2790457a3c1c50d66
Stanislas <avotraStani@gmail.com>
2026-09-11 10:08:38 +0300

    Fix update directive angular

--- src/app/ai-chat/ai-chat.ts
+++ src/app/ai-chat/ai-chat.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -9,6 +9,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './ai-chat.html',
   styleUrl: './ai-chat.css',
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class AiChat {
   isOpen = false;
   messages: { sender: string; text: string }[] = [];
@@ -30,8 +35,12 @@ export class AiChat {
   getChatResponse(message: string) {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
+    interface ChatResponse {
+        response: string;
+      }
+
     this.httpClient
-      .post<any>('http://127.0.0.1:8000/chat', { message }, { headers })
+      .post<ChatResponse>('http://127.0.0.1:8000/chat', { message }, { headers })
       .subscribe({
         next: (response) => {
           this.messages.push({ sender: 'ai', text: response.response });
